8e22994197588c0d80a56821548eb0d32b00bd77
Zach Jones <zachtjones16@gmail.com>
2016-12-24 17:57:23 -0500

    Is now an instance-based class. The main class has a hashMap of definitions.

--- src/Definition.java
+++ src/Definition.java
@@ -1,33 +1,62 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Serializable;
 import java.net.URL;
 import java.util.Scanner;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.KeyListener;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.FileDialog;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
-
-public class Definition {
+public class Definition implements Comparable<Definition>, Serializable {
+	
+	/** The version ID for this definition */
+	private static final long serialVersionUID = 1L;
+	
+	private String word;
+	private String definition;
+	
+	/**
+	 * Creates the definition of a word from an online dictionary.
+	 * @param word The String that is the word to define.
+	 * @throws IOException This will throw an IOException if the word is not found online, 
+	 * no netword connection, or any other I/O error.
+	 */
+	public Definition(String word) throws IOException {
+		this.word = word;
+		this.definition = defineWord(word);
+	}
+	
+	/**
+	 * Creates a definition of a word give both the word and the definition.
+	 * This is used for allowing the user to define words.
+	 * @param word The String that is the word to define.
+	 * @param definition The definition of the word.
+	 */
+	public Definition(String word, String definition){
+		this.word = word;
+		this.definition = definition;
+	}
+	
+	/**
+	 * Gets the word for this definition.
+	 * @return The word that this definition defines.
+	 */
+	public String getWord(){
+		return this.word;
+	}
+	
+	/**
+	 * Gets the definition of the word.
+	 * @return The string that is the definition of the word.
+	 */
+	public String getDefinition(){
+		return this.definition;
+	}
+	
 	/**
 	 * Downloads a page as a String from the URL
 	 * @param url The page location
 	 * @return A string that is the contents of the page
 	 * @throws IOException If there is an error either discovering the page or reading from it
 	 */
-	static String downloadPage(URL url) throws IOException{
+	private static String downloadPage(URL url) throws IOException{
 		String allText = "";
 		InputStream reader = url.openStream();
 		Scanner sc = new Scanner(reader);
@@ -37,16 +66,18 @@ public class Definition {
 		sc.close();
 		return allText;
 	}
+	
 	/**
 	 * Downloads a page as a String from the String fileName that is the represetation of the URL (such as http://www.google.com)
 	 * @param fileName The URL as a String
 	 * @return A string that is the contents of the page
 	 * @throws IOException If there is an error either discovering the page or reading from it
 	 */
-	static String downloadPage(String fileName) throws IOException{
+	private static String downloadPage(String fileName) throws IOException{
 		URL url = new URL(fileName);
 		return downloadPage(url);
 	}
+	
 	/**
 	 * Gets the definition of a word. Also saves the word to file in the definitions folder if the definition exists
 	 * @param word The String that is the word to define
@@ -54,7 +85,7 @@ public class Definition {
 	 * @throws IOException If an IOException occurs
 	 * @throws Exception If another type of Exception occurs that is not an IOException
 	 */
-	static String defineWord(String word) throws IOException, Exception{
+	private static String defineWord(String word) throws IOException {
 		String fileName = "http://dictionary.reference.com/browse/" + word;
 		String allContents = downloadPage(fileName);
 		int startPos = allContents.indexOf("<div class=\"source-data\">");
@@ -92,9 +123,9 @@ public class Definition {
 		//replace \n with \r\n so it works on windows line terminators
 		rest = rest.replace("\n", "\r\n");
 		//save the def to file, even if there is already a definition for the word
-		saveToFile(word, rest);
 		return rest;
 	}
+	
 	/**
 	 * Private method that returns a string that goes from '<' to the next '>' char. Useful for removing HTML tags. <br>
 	 * This method is used potentially many times in the defineWord method
@@ -111,111 +142,15 @@ public class Definition {
 		}
 		return "";
 	}
+	
 	/**
-	 * Defines a word using the defineWord(String) method, showing a shell (window) that has the definition typed in.
-	 * @param display The display that is for this program.
-	 * @param shell Only used to set the location of this new window to the same as the other window. Must not be null
+	 * Gets if a word is a roman numeral.
+	 * A roman numeral does not have to be properly formatter, however it must only contain:
+	 * I, V, X, L, C, D, and M characters.
+	 * @param word The string that is the word to check.
+	 * @return true iff word is a roman numeral
 	 */
-	static void defineWord(){
-		//TODO create an alert instead
-		
-		final Shell newShell = new Shell(display);
-		newShell.setSize(600, 400);
-		newShell.setText("Define a word");
-		newShell.setLocation(shell.getLocation());
-		Label newLabel = new Label(newShell, SWT.NONE);
-		newLabel.setText("Enter the word and then click define");
-		newLabel.setBounds(5, 5, 300, 25);
-		final Text txt = new Text(newShell, SWT.SINGLE);
-		final Text txtDef = new Text(newShell, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
-		txt.setBounds(5, 30, 200, 20);
-		txt.addKeyListener(new KeyListener(){
-
-			@Override
-			public void keyPressed(KeyEvent e) {
-				if(e.keyCode == 13){
-					String word = txt.getText();
-					try {
-						String result = defineWord(word);
-						txtDef.setText(result);
-					} catch (Exception e1) {
-						MessageBox mb = new MessageBox(newShell, SWT.OK);
-						mb.setMessage("Error downloading page");
-						mb.setText("Error");
-						mb.open();
-					}
-				}
-			}
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-				//don't need to do anything here
-			}
-			
-		});
-		txtDef.setEditable(false);
-		txtDef.setBounds(5, 60, 570, 290);
-		
-		Button newButton = new Button(newShell, SWT.PUSH);
-		newButton.setBounds(210, 30, 100, 20);
-		newButton.setText("Define");
-		newButton.addSelectionListener(new SelectionListener(){
-
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				String word = txt.getText();
-				try {
-					String result = defineWord(word);
-					txtDef.setText(result);
-				} catch (Exception e1) {
-					MessageBox mb = new MessageBox(newShell, SWT.OK);
-					mb.setMessage("Error downloading page");
-					mb.setText("Error");
-					mb.open();
-				}
-			}
-
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {
-				//not called
-			}
-			
-		});
-		newShell.open();
-		while(!newShell.isDisposed()){
-			if(!display.readAndDispatch()){
-				display.sleep();
-			}
-		}
-	}
-	
-	static String defineWordUsingFileIfExists(String word) throws IOException, Exception{
-		File tempFile = new File(NotesMain.defDir + word + ".txt");
-		if(tempFile.exists()){
-			try {
-				String definition = readAll(word);
-				return definition;
-			} catch (IOException e) {
-				
-			}
-		}
-		//if the file doesn't exist then use the defineWord(String)
-		return defineWord(word);
-		
-	}
-	
-	static String readAll(String fileName) throws IOException{
-		FileReader fr = new FileReader(fileName);
-		String result = "";
-		int tChar = 0;
-		while((tChar = fr.read()) != -1){
-			result += "" + (char)tChar;
-		}
-		fr.close();
-		return result;
-	}
-	
-	static boolean isRomanNumeral(String word){
+	public static boolean isRomanNumeral(String word){
 		//if all the characters in the word are roman numerals, then it is considered a Roman Numeral
 		String possible_letters = "IVXLCDM";
 		for(int i = 0; i < word.length(); i++){
@@ -225,62 +160,16 @@ public class Definition {
 		}
 		return true;
 	}
+
 	/**
-	 * Saves the definition of a word to file, printing the error to the standard output stream (System.out) if any Exceptions occur.
-	 * @param word The word that the definition is for. Note that the file that is saved to is the NotesMain.defDir + word + ".txt"
-	 * @param contents The contents as a string of the definition.
+	 * Required for the natural ordering of Definitions.
+	 * This compares the words of the definitons, ignoring case.
+	 * @param o The other definition to compare to.
+	 * @return a negative integer, zero, or a positive integer as the specified Definition is 
+	 * greater than, equal to, or less than this Definition, ignoring case considerations.
 	 */
-	static void saveToFile(String word, String contents){
-		//save to file
-		try {
-			File newFile = new File(NotesMain.defDir + word + ".txt");
-			System.out.println("Log: adding a definition for word: " + newFile.getAbsolutePath());
-			File dir = new File(NotesMain.defDir);
-			if(!dir.isDirectory()){
-				dir.mkdirs();
-			}
-			if(!newFile.exists()){
-				FileWriter fw = new FileWriter(newFile);
-				BufferedWriter bw = new BufferedWriter(fw);
-				bw.write(contents);
-				bw.flush();
-				bw.close();
-				fw.close();
-			}
-		} catch (Exception e) {
-			System.out.println("Error saving the definition of: " + word + " to file.");
-		}
-	}
-	/**
-	 * Creates an archive file of the definitions, prompting the user for the location to save the archive file
-	 * <br>The archive file format is as follows:
-	 * number of definitions + "\n"
-	 * n lines consisting of "word.txt" + file.length(), where n is the number of definitions
-	 * the contents of the files (no extra stuff added) the length devoted to each definition is the number at the header
-	 * @param parent The parent shell to show both the FileDailog and MessageBox over
-	 */
-	static void createDefArchive(){
-		FileDialog fd = new FileDialog(parent, SWT.SAVE);
-		String[] temp1 = {"*.defnar"};
-		String[] temp2 = {"Definitions Archive file (*.defnar)"};
-		fd.setFilterExtensions(temp1);
-		fd.setFilterNames(temp2);
-		String resultFn = fd.open();
-		ArchiveThread at = new ArchiveThread(resultFn, ArchiveThread.Values.WRITE, parent);
-		at.start();
-	}
-	/**
-	 * Creates the definitions from the archive file
-	 * @param parent The
-	 */
-	static void loadArchive(){
-		FileDialog fd = new FileDialog(parent, SWT.OPEN);
-		String[] temp1 = {"*.defnar"};
-		String[] temp2 = {"Definitions Archive file (*.defnar)"};
-		fd.setFilterExtensions(temp1);
-		fd.setFilterNames(temp2);
-		String resultFn = fd.open();
-		ArchiveThread at = new ArchiveThread(resultFn, ArchiveThread.Values.READ, parent);
-		at.start();
+	@Override
+	public int compareTo(Definition o) {
+		return this.word.compareToIgnoreCase(o.word);
 	}
 }
